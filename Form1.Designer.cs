a58fb70f8aa387cdf43ea9ba7cfcd039cbf12197
ArunKumar M <arunm1917@gmail.com>
2016-01-07 16:46:14 +0530

    first one

---
+++ Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace student
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.cmdsave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtdesp = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.txtdob = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.despDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dobDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.course12BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.arunDataSet1 = new student.arunDataSet1();
+            this.cmdclose = new System.Windows.Forms.Button();
+            this.arunDataSet = new student.arunDataSet();
+            this.course12TableAdapter = new student.arunDataSet1TableAdapters.course12TableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.course12BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arunDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arunDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(209, 55);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(100, 20);
+            this.txtid.TabIndex = 2;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(209, 99);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 20);
+            this.txtname.TabIndex = 3;
+            // 
+            // cmdsave
+            // 
+            this.cmdsave.Location = new System.Drawing.Point(340, 52);
+            this.cmdsave.Name = "cmdsave";
+            this.cmdsave.Size = new System.Drawing.Size(75, 23);
+            this.cmdsave.TabIndex = 4;
+            this.cmdsave.Text = "Save";
+            this.cmdsave.UseVisualStyleBackColor = true;
+            this.cmdsave.Click += new System.EventHandler(this.cmdsave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "NAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "DESCRIPTION";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "EMAIL ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(74, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "DOB";
+            // 
+            // txtdesp
+            // 
+            this.txtdesp.Location = new System.Drawing.Point(209, 142);
+            this.txtdesp.Name = "txtdesp";
+            this.txtdesp.Size = new System.Drawing.Size(100, 20);
+            this.txtdesp.TabIndex = 10;
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(209, 187);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(100, 20);
+            this.txtemail.TabIndex = 11;
+            // 
+            // txtdob
+            // 
+            this.txtdob.Location = new System.Drawing.Point(211, 233);
+            this.txtdob.Name = "txtdob";
+            this.txtdob.Size = new System.Drawing.Size(100, 20);
+            this.txtdob.TabIndex = 12;
+            this.txtdob.TextChanged += new System.EventHandler(this.txtdob_TextChanged);
+            this.txtdob.Validating += new System.ComponentModel.CancelEventHandler(this.txtdob_Validating);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.despDataGridViewTextBoxColumn,
+            this.emailDataGridViewTextBoxColumn,
+            this.dobDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.course12BindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 276);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(551, 150);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // despDataGridViewTextBoxColumn
+            // 
+            this.despDataGridViewTextBoxColumn.DataPropertyName = "desp";
+            this.despDataGridViewTextBoxColumn.HeaderText = "desp";
+            this.despDataGridViewTextBoxColumn.Name = "despDataGridViewTextBoxColumn";
+            // 
+            // emailDataGridViewTextBoxColumn
+            // 
+            this.emailDataGridViewTextBoxColumn.DataPropertyName = "email";
+            this.emailDataGridViewTextBoxColumn.HeaderText = "email";
+            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+            // 
+            // dobDataGridViewTextBoxColumn
+            // 
+            this.dobDataGridViewTextBoxColumn.DataPropertyName = "dob";
+            this.dobDataGridViewTextBoxColumn.HeaderText = "dob";
+            this.dobDataGridViewTextBoxColumn.Name = "dobDataGridViewTextBoxColumn";
+            // 
+            // course12BindingSource
+            // 
+            this.course12BindingSource.DataMember = "course12";
+            this.course12BindingSource.DataSource = this.arunDataSet1;
+            // 
+            // arunDataSet1
+            // 
+            this.arunDataSet1.DataSetName = "arunDataSet1";
+            this.arunDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cmdclose
+            // 
+            this.cmdclose.Location = new System.Drawing.Point(340, 116);
+            this.cmdclose.Name = "cmdclose";
+            this.cmdclose.Size = new System.Drawing.Size(75, 23);
+            this.cmdclose.TabIndex = 14;
+            this.cmdclose.Text = "close";
+            this.cmdclose.UseVisualStyleBackColor = true;
+            this.cmdclose.Click += new System.EventHandler(this.cmdclose_Click);
+            // 
+            // arunDataSet
+            // 
+            this.arunDataSet.DataSetName = "arunDataSet";
+            this.arunDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // course12TableAdapter
+            // 
+            this.course12TableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 471);
+            this.Controls.Add(this.cmdclose);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtdob);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.txtdesp);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdsave);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.course12BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arunDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arunDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Button cmdsave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtdesp;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.TextBox txtdob;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button cmdclose;
+        private arunDataSet arunDataSet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn despDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dobDataGridViewTextBoxColumn;
+        private arunDataSet1 arunDataSet1;
+        private System.Windows.Forms.BindingSource course12BindingSource;
+        private arunDataSet1TableAdapters.course12TableAdapter course12TableAdapter;
+    }
+}
+
